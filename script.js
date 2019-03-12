0ffff1815b88bbd3200c25eb2ea32a343bd3c3d6
Geoff Gorham <geogor37@gmail.com>
2019-03-12 13:59:35 -0600

    Added the ability to add items to the selected list

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ var app = new Vue({
   data: {
     lists: [],
     newListName: '',
+    selectedList: {},
+    newItemName: '',
   },
   methods: {
     createList() {
       this.lists.push({ name: this.newListName, items: [] });
       this.newListName = '';
+    },
+    selectList(list) {
+      this.selectedList = list;
+    },
+    addItemToList() {
+      this.selectedList.items.push(this.newItemName);
+      this.newItemName = '';
     },
   },
 });
